309c7c9ce881ea3c3514afb998c19380d92cf5bd
Aryan2745 <kr.aryan2745@gmail.com>
2024-08-03 13:26:14 +0530

    docker creation

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12.0
+RUN apt update -y && apt install awscli -y
+WORKDIR /app
+
+COPY . /app
+RUN pip install -r requirements.txt
+CMD ["python","app.py"]
--- main.py
+++ main.py
@@ -15,11 +15,15 @@ from fastapi.responses import Response
 from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
 from sensor.utils.main_utils import load_object
 from fastapi.middleware.cors import CORSMiddleware
-env_file_path=os.environ['MONGO_DB_URL']
+import os
 
-#def set_env_variable(env_file_path):
-  #  env_config = read_yaml_file(env_file_path)
-  #  os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
 
 app = FastAPI()
@@ -74,7 +78,7 @@ async def predict_route():
 
 def main():
     try:
-        #set_env_variable(env_file_path)
+        set_env_variable(env_file_path)
         training_pipeline = TrainPipeline()
         training_pipeline.run_pipeline()
     except Exception as e:
@@ -83,7 +87,6 @@ def main():
 
 
 if __name__=="__main__":
-    #main() 
-    app_run(app, host=APP_HOST, port=APP_PORT)
+    #main()
     # set_env_variable(env_file_path)
-    # app_run(app, host=APP_HOST, port=APP_PORT)
+    app_run(app, host=APP_HOST, port=APP_PORT)
